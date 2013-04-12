699a2c09c1ff01a2911a0479132ef98aa8724786
Beeccy <gjereb12@nith.no>
2013-04-12 19:28:28 +0200

    Revert "Comment"
    This reverts commit 112d083616697389269a7bdd7ca09eea7599a06d.

--- MyLittlePonySlenderMan/MyLittlePonySlenderMan/Game1.cs
+++ MyLittlePonySlenderMan/MyLittlePonySlenderMan/Game1.cs
@@ -99,7 +99,7 @@ namespace MyLittlePonySlenderMan
 
         protected override void Update(GameTime gameTime)
         {
-            //Allows the game to exit
+            // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
